49f8256677e01e185737e05e29b48320b1fceacc
JurgensGH <igor.voznyuk.97@gmail.com>
2024-09-30 22:23:37 +0300

    add icon

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from pygame.examples.go_over_there import running
 
 pygame.init()
 
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Shooting Gallery")
+icon = pygame.image.load("image/icon.jpg")
+
+
 running = True
 while running:
-    running = not running
+    pass
 
 pygame.quit()
