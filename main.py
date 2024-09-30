bc7f035bdd767709791add99f7a8102888600c07
JurgensGH <igor.voznyuk.97@gmail.com>
2024-09-30 22:40:04 +0300

    add images

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.examples.go_over_there import running
+import random
+import time
 
 pygame.init()
 
@@ -9,7 +11,15 @@ screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("Shooting Gallery")
 icon = pygame.image.load("image/icon.jpg")
+pygame.display.set_icon(icon)
 
+target_img = pygame.image.load("image/target.png")
+target_width = 50
+target_height = 50
+target_x = random.randint(0, screen_width - target_width)
+target_y = random.randint(0, screen_height - target_height)
+
+color = (355, 355, 355)
 
 running = True
 while running:
